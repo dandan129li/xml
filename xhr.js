91320c214ec07983d4011939796b8949c480dc79
dandan129li <dandan129li@gmail.com>
2017-03-20 11:51:01 +0800

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -19,5 +19,16 @@ function createXHR(){
     throw new Error("No XHR object available.");
   }
 }
-
-
+var xhr = new createXHR();
+xhr.onreadystatechange = function(){
+  if(xhr.readyState == 4){
+    if((xhr.status>==200 && xhr.status<300)|| xhr.status-- 304){
+      alert(xhr.responseText);
+    }else{
+      alert("Request was unsuccessful:" + xhr.status);
+    }
+  }
+};
+xhr.open("get","example.txt",true);
+xhr.send(null);//参数 无传null
+//xhr.abort(); //取消异步请求
